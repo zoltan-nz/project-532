8ad5dddd76883d1014f807580232e71ce6073b5c
Zoltan <zdebre@gmail.com>
2013-04-01 23:57:29 +0100

    products view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,9 +2,14 @@ class ProductsController < ApplicationController
   layout 'products'
 
   def index
+
   end
 
   def show
+    @departments = Department.all
+    @categories = Category.all
+    @products = Product.all
+
 
   end
 end
