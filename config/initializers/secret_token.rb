10d6413b3734d81ef6c2579aa37b3d59f6183872
Zoltan <zdebre@gmail.com>
2013-04-08 15:32:07 +0100

    secret token update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Project532::Application.config.secret_token = '1f3d9e0dd17156b96be1d931a9b51661799d03597e331d3b371817d47f166d710a53d6bee55f50aa14f6ee6379dcf253c021fa878a0e9ffb112972aa1ea9030d'
+Project532::Application.config.secret_key_base = '1f3d9e0dd17156b96be1d931a9b51661799d03597e331d3b371817d47f166d710a53d6bee55f50aa14f6ee6379dcf253c021fa878a0e9ffb112972aa1ea9030d'
